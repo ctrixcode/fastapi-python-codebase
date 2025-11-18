a403608fb1b633a4796b69c2aec5adf5466044ee
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-11-18 16:40:25 +0530

    feat: added cors all

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from typing import Dict
 from .schemas.health import HealthCheck
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="FastAPI Python Codebase", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+)
 
 
 @app.get(
