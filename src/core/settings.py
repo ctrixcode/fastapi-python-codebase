695e7e862a144e06b0226320826f796b20262388
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-11-18 17:12:31 +0530

    feat: get settings cached

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,6 +1,12 @@
 from pydantic_settings import BaseSettings
+from functools import lru_cache
 
 
 class Settings(BaseSettings):
     app_name: str = "FastAPI Python Codebase"
     app_version: str = "1.0.0"
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
