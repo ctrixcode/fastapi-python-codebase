7ade9b10da4453a5038974ed1dbb561274c3d3e4
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-11-23 15:12:48 +0530

    feat: responses

---
+++ src/schemas/response.py
@@ -0,0 +1,16 @@
+from pydantic.generics import GenericModel
+from pydantic import BaseModel, Field
+from typing import TypeVar, Generic
+
+T = TypeVar("T")
+
+
+class SuccessResponse(GenericModel, Generic[T]):
+    success: bool = Field(default=True, description="Response status")
+    data: T = Field(..., description="Response data")
+
+
+class ErrorResponse(BaseModel):
+    success: bool = Field(default=False, description="Response status")
+    err_code: str = Field(..., description="Error code")
+    message: str = Field(..., description="Error message")
